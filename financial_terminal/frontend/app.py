ff376bbb4ef8680734f6c8496da9165ce14377df
Fernando Doc <81945040+fernandodoc@users.noreply.github.com>
2026-02-05 02:51:37 -0300

    Update app.py

--- financial_terminal/frontend/app.py
+++ financial_terminal/frontend/app.py
@@ -46,7 +46,7 @@ def main():
     
     # --- SIDEBAR NAVEGAÇÃO ---
     st.sidebar.markdown("<h1 style='text-align: center;'>🛡️</h1>", unsafe_allow_html=True)
-    st.sidebar.markdown("<h3 style='text-align: center; color: #f3d007;'>TERMINAL VISION PRO</h3>", unsafe_allow_html=True)
+    st.sidebar.markdown("<h3 style='text-align: center; color: #f3d007;'>TERMINAL PRO VISION</h3>", unsafe_allow_html=True)
         
     # Mapeamento atualizado com ETFs e FIIs
     menu_options = {
@@ -187,6 +187,7 @@ if __name__ == "__main__":
     st.sidebar.caption("Idealizado por Fernando | Profissional com certificações ANBIMA (C-PRO I • C-PRO R) e ANCORD")
     
     
+
 
 
 
