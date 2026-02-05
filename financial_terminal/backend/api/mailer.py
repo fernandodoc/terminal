55098a010a6ac71aa05aa5b68360969f0040cb31
Fernando Doc <81945040+fernandodoc@users.noreply.github.com>
2026-02-05 17:30:31 -0300

    Create mailer.py
    formspress - sending/receiving email

---
+++ financial_terminal/backend/api/mailer.py
@@ -0,0 +1,21 @@
+import requests
+
+def enviar_dados_ao_especialista(dados):
+    # CERTIFIQUE-SE de que a URL termina com o ID do seu formulário
+    # Exemplo: https://formspree.io/f/mqkvpkn
+    FORMSPREE_URL = "https://formspree.io/f/xgolwpkw"
+    
+    try:
+        # Adicionamos headers para garantir que a API entenda que é um formulário
+        response = requests.post(
+            FORMSPREE_URL, 
+            data=dados,
+            timeout=10 # Evita que o site fique travado se a conexão cair
+        )
+        
+        # O Formspree retorna 200 se for sucesso
+        return response.status_code == 200
+    except Exception as e:
+        # Isso imprimirá o erro real no seu terminal do VS Code para debug
+        print(f"Erro detalhado na conexão: {e}")
+        return False
